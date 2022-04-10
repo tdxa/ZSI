bd258881bf0bf22f75df634015e6920a7b0361cf
tdxa <anndoman3@gmail.com>
2022-04-11 00:05:24 +0200

    Roulette & tournament selection

--- ok.py
+++ ok.py
@@ -35,7 +35,6 @@ def rate(matrix: List[List[int]], population: List[List[int]]):
         rates_uq = []
         try:
             for _ in individual:
-                print(matrix[individual[n]][individual[n + 1]])
                 rates_uq.append(matrix[individual[n]][individual[n + 1]])
                 n += 1
         except:
@@ -46,3 +45,46 @@ def rate(matrix: List[List[int]], population: List[List[int]]):
     return [[population[i], rates[i]] for i in range(len(population))]
 
 
+def reverse_individual_rating(data):
+    max_rate = max([x[1] for x in data])
+    return [max_rate + 1 - x[1] for x in data]
+
+
+def selection_tournament(data):
+    tournament = [random.sample(data, 3) for _ in range(5)]
+    ratings = []
+
+    for i in range(len(tournament)):
+        ratings.append([c[1] for c in tournament[i]])
+
+    winners_indexes = [c.index(min(c)) for c in ratings]
+
+    new_population = []
+    for i in range(len(tournament)):
+        new_population.append(tournament[i][winners_indexes[i]])
+
+    return new_population
+
+
+def selection_roulette(data):
+    new_rating = reverse_individual_rating(data)
+    sum_rating = sum(new_rating)
+    probabilities = [p / sum_rating for p in new_rating]
+    new_pop = random.choices(data, probabilities, k=4)
+
+    return new_pop
+
+
+def genetic_algorithm(file, epochs):
+    pass
+
+
+mat = create_matrix(read_file(f))
+pop = generate_base_population(mat, 5)
+pop_rat = rate(mat, pop)
+# print(mat)
+# print(pop)
+# print(pop_rat)
+
+selection_roulette(pop_rat)
+selection_tournament(pop_rat)
